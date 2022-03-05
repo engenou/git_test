8f518acde8e4cadf67ebe02d8c28860d4a370491
Eugen <Eugen@google.com>
2022-03-05 14:30:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Eugen
+ * @creat 2022-03-05 14:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
